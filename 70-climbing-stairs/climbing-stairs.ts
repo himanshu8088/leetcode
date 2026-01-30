d9b40142f08009aee3d9b01187bde82d5d5afc40
Himanshu Soni <himanshusoni1234@gmail.com>
2026-01-30 06:04:51 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (49.87%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,12 +1,12 @@
-let dp = [];
 function climbStairs(n: number): number {
+    const dp = [];
     for(let i=0; i<n; i++){
         dp[i] = -1;
     }
-    return climb(0, n);
+    return climb(0, n, dp);
 };
 
-function climb(stair: number, n: number){
+function climb(stair: number, n: number, dp: number[]){
     if(stair === n) {
         return 1;
     }
@@ -17,6 +17,6 @@ function climb(stair: number, n: number){
         return dp[stair];
     }
     
-    dp[stair] = climb(stair+1, n) + climb(stair+2, n);
+    dp[stair] = climb(stair+1, n, dp) + climb(stair+2, n, dp);
     return dp[stair];
 }
